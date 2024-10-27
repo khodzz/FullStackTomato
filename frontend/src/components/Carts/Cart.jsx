4d854d86377e6c913e0161a5466454ac34f28a8d
Islambek Khodzhaev <khodzhaevislambek@gmail.com>
2024-10-27 12:25:53 +0200

    frontend

---
+++ frontend/src/components/Carts/Cart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div>
+      asdasd
+      <div className="asdasdasd">asdasdasd</div>
+    </div>
+  );
+};
+
+export default Cart;
